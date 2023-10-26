f05c2392523ca25faa882e3c1c56e96d47c63ed3
Antonedei Wonimidei Otoro <antonedei.otoro@engineer.com>
2023-10-25 23:04:27 -0500

    refactored messageService

--- backend/CrossingService/src/main/java/com/miu/eventtrackerapi/Service/MessageService.java
+++ backend/CrossingService/src/main/java/com/miu/eventtrackerapi/Service/MessageService.java
@@ -28,39 +28,7 @@ public class MessageService {
 
     public Collection<DataApi> getMessagesFromTopic(String topicName) {
         try {
-            // Create an ObjectMapper
-            ObjectMapper objectMapper = new ObjectMapper();
-            return retriever.getAllFromTopic(topicName, Duration.ofSeconds(1));git
-//                    .stream()
-//                    .filter(r-> {
-//                        System.out.println("Here it is: " + r);
-                        // Parse the JSON string into a JsonNode
-//                        JsonNode jsonNode = null;
-//                        String jsonString;
-//                        try {
-//                            jsonString = objectMapper.writeValueAsString(r);
-//                        } catch (JsonProcessingException e) {
-//                            throw new RuntimeException(e);
-//                        }
-//                        try {
-//                            jsonNode = objectMapper.readTree(String.valueOf(r));
-//                        } catch (JsonProcessingException e) {
-//                            throw new RuntimeException(e);
-//                        }
-
-                        // Accessing values from the JsonNode
-//                        String url = jsonNode.get("url").asText();
-//                        String apiKey = jsonNode.get("apiKey").asText();
-//                        boolean needsKey = jsonNode.get("needsKey").asBoolean();
-
-//                        var api = new DataApi();
-//                        api.setApiKey(r.getApiKey());
-//                        api.setUrl(r.getUrl());
-//                        api.setNeedsKey(r.isNeedsKey());
-//                        return api;
-//                        return r;
-//                        return false;
-//                    }).toList();
+            return retriever.getAllFromTopic(topicName, Duration.ofSeconds(1));
         } catch (Exception e) {
             e.printStackTrace();
             return null;
