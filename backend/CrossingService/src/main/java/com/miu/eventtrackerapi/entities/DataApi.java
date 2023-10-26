20c06b7373cd211430556eadfb2a34a8c7ef8ade
gl0rious <majidgues@gmail.com>
2023-10-26 10:21:13 -0500

    added id field

--- backend/CrossingService/src/main/java/com/miu/eventtrackerapi/entities/DataApi.java
+++ backend/CrossingService/src/main/java/com/miu/eventtrackerapi/entities/DataApi.java
@@ -13,6 +13,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 @Data
 public class DataApi {
+    private Long id;
     private String url;
     private String apiKey;
     private boolean needsKey;
